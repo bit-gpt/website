0386b7bdd8eec510eb2396cf02de542fa4f12ee2
Janaka-Steph <kakobaba1212@protonmail.com>
2024-01-25 15:20:06 +0100

    remove unused fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,8 @@ import type {Metadata} from 'next'
 import {Nunito, Outfit, PT_Sans, Sen} from 'next/font/google'
 import './globals.css'
 
-const sen = Sen({subsets: ['latin'], display: 'swap', variable: '--font-sen'})
-const outfit = Outfit({subsets: ['latin'], display: 'swap', variable: '--font-outfit'})
 const nunito = Nunito({subsets: ['latin'], display: 'swap', variable: '--font-nunito'})
 const ptSans = PT_Sans({subsets: ['latin'], display: 'swap', variable: '--font-pt-sans', weight: "400"})
-
 
 export const metadata: Metadata = {
   title: 'BitGPT',
@@ -18,7 +15,7 @@ export default function RootLayout({children}: {
 }) {
   return (
     <html lang="en">
-    <body className={`${sen.variable} ${outfit.variable} ${nunito.variable} ${ptSans.variable}`}>{children}</body>
+    <body className={`${nunito.variable} ${ptSans.variable}`}>{children}</body>
     </html>
   )
 }
